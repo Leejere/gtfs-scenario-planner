c4be83c28e038c5e3590204ffb2371e4f3873699
Leejere <li.jie.urban@gmail.com>
2023-05-15 08:45:19 -0400

    moved index.html to src

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+  },
   server: {
     host: true,
     strictPort: true,
